f8a72488b65ebe6b47c452034ae1234b5834aec6
Abhisar Mohapatra <abhisar.mohapatra@walmart.com>
2023-08-08 05:53:12 +0530

    Scanner coding

---
+++ src/main/java/com/abhisar/interpreter/TokenType.java
@@ -0,0 +1,21 @@
+package com.abhisar.interpreter;
+
+public enum TokenType {
+    //Single Character token
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    //1 or 2 character tokens.
+    BANG,BANG_EQUAL,EQUAL,EQUAL_EQUAL,GREATER,
+    GREATER_EQUAL,LESS,LESS_EQUAL,
+
+    //Literals
+    IDENTIFIER,STRING,NUMBER,
+
+    //KEYWORDS
+    AND,CLASS,ELSE,FALSE,FUN,FOR,IF,NIL,OR,
+    PRINT,RETURN,SUPER,THIS,TRUE,VAR,WHILE,
+
+    //End of file
+    EOF
+}
